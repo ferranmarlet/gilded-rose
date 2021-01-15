4a87d256b9beaed2f711839d9167e962143162e7
Ferran Marlet <ferran.marlet@holaluz.com>
2021-01-15 12:28:30 +0100

    Add UpdateItemService and move update responsibility into it

--- php/tests/GildedRoseTest.php
+++ php/tests/GildedRoseTest.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace Tests;
 
+use GildedRose\GenericItem;
 use GildedRose\GildedRose;
 use GildedRose\Item;
+use GildedRose\UpdateItemService;
 use PHPUnit\Framework\TestCase;
 
 class GildedRoseTest extends TestCase
@@ -15,6 +17,16 @@ class GildedRoseTest extends TestCase
         $items = [new Item('foo', 0, 0)];
         $gildedRose = new GildedRose($items);
         $gildedRose->updateQuality();
-        $this->assertSame('fixme', $items[0]->name);
+        $this->assertSame('foo', $items[0]->name);
     }
+
+    public function testGenericItemPropertiesDecreaseWhenUpdated(): void
+    {
+        $item = new Item('foo', 5, 7);
+        UpdateItemService::execute($item);
+
+        $this->assertEquals($item->sell_in, 4);
+        $this->assertEquals($item->quality, 6);
+    }
+
 }
