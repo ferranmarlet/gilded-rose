9dfc4d5e3f250cb9fbf8a7e490c1b5cd0d65cb5d
xafardero <xavier.mabras@protonmail.ch>
2021-01-22 08:46:52 +0100

    Modify GenericItemStrategy.
    We where using this as a fallback class to refactor in a easier way

--- php/src/UpdateGenericItemStrategy.php
+++ php/src/UpdateGenericItemStrategy.php
@@ -6,50 +6,14 @@ class UpdateGenericItemStrategy implements UpdateItemStrategyInterface
 {
     public static function update(Item $item): void
     {
-        if ($item->name !== 'Aged Brie' and $item->name !== 'Backstage passes to a TAFKAL80ETC concert') {
-            if ($item->quality > 0) {
-                if ($item->name !== 'Sulfuras, Hand of Ragnaros') {
-                    $item->quality--;
-                }
-            }
+        if ($item->sell_in <= 0) {
+            $item->quality -= 2;
         } else {
-            if ($item->quality < 50) {
-                $item->quality++;
-                if ($item->name === 'Backstage passes to a TAFKAL80ETC concert') {
-                    if ($item->sell_in < 11) {
-                        if ($item->quality < 50) {
-                            $item->quality++;
-                        }
-                    }
-                    if ($item->sell_in < 6) {
-                        if ($item->quality < 50) {
-                            $item->quality++;
-                        }
-                    }
-                }
-            }
+            $item->quality--;
         }
 
-        if ($item->name !== 'Sulfuras, Hand of Ragnaros') {
-            $item->sell_in--;
-        }
-
-        if ($item->sell_in < 0) {
-            if ($item->name !== 'Aged Brie') {
-                if ($item->name !== 'Backstage passes to a TAFKAL80ETC concert') {
-                    if ($item->quality > 0) {
-                        if ($item->name !== 'Sulfuras, Hand of Ragnaros') {
-                            $item->quality--;
-                        }
-                    }
-                } else {
-                    $item->quality = 0;
-                }
-            } else {
-                if ($item->quality < 50) {
-                    $item->quality++;
-                }
-            }
-        }
+        $item->sell_in--;
+    
+        $item->quality = max(0, $item->quality);
     }
 }
